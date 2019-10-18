25b2544080d6834065ac6bf0e9b67a1e998ec19e
Luiz Guilherme Silva de Jesus_CIGAM Prodaly <Luiz@prodaly.com.br>
2019-10-18 11:29:35 -0300

    Adicionar arquivos de projeto.

---
+++ LerVariaveisMergeArquivo/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace LerVariaveisMergeArquivo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnOpenFolder = new System.Windows.Forms.Button();
+            this.btnProcessar = new System.Windows.Forms.Button();
+            this.txtDelimitador = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(85, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Arquivo:";
+            // 
+            // txtFile
+            // 
+            this.txtFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFile.Location = new System.Drawing.Point(151, 30);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(366, 23);
+            this.txtFile.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnOpenFolder
+            // 
+            this.btnOpenFolder.Location = new System.Drawing.Point(523, 30);
+            this.btnOpenFolder.Name = "btnOpenFolder";
+            this.btnOpenFolder.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenFolder.TabIndex = 2;
+            this.btnOpenFolder.Text = "Open";
+            this.btnOpenFolder.UseVisualStyleBackColor = true;
+            this.btnOpenFolder.Click += new System.EventHandler(this.BtnOpenFolder_Click);
+            // 
+            // btnProcessar
+            // 
+            this.btnProcessar.Location = new System.Drawing.Point(151, 92);
+            this.btnProcessar.Name = "btnProcessar";
+            this.btnProcessar.Size = new System.Drawing.Size(75, 23);
+            this.btnProcessar.TabIndex = 3;
+            this.btnProcessar.Text = "Processar";
+            this.btnProcessar.UseVisualStyleBackColor = true;
+            this.btnProcessar.Click += new System.EventHandler(this.BtnProcessar_Click);
+            // 
+            // txtDelimitador
+            // 
+            this.txtDelimitador.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDelimitador.Location = new System.Drawing.Point(152, 64);
+            this.txtDelimitador.Name = "txtDelimitador";
+            this.txtDelimitador.Size = new System.Drawing.Size(27, 22);
+            this.txtDelimitador.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(62, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Delimitador:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 134);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(792, 388);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(232, 92);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(285, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar1.TabIndex = 7;
+            this.progressBar1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(816, 548);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.txtDelimitador);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnProcessar);
+            this.Controls.Add(this.btnOpenFolder);
+            this.Controls.Add(this.txtFile);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Extrai Variaveis Merge";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnOpenFolder;
+        private System.Windows.Forms.Button btnProcessar;
+        private System.Windows.Forms.TextBox txtDelimitador;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ LerVariaveisMergeArquivo/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LerVariaveisMergeArquivo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        StringBuilder strVariaveis = new StringBuilder();
+        private void BtnOpenFolder_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            txtFile.Text = openFileDialog1.FileName;
+        }
+
+        private void BtnProcessar_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(txtFile.Text))
+            {
+                strVariaveis.Clear();
+                progressBar1.Visible = true;
+                processarArquivo(txtFile.Text, txtDelimitador.Text);
+
+                richTextBox1.Text = "";
+                richTextBox1.Text = strVariaveis.ToString();
+                progressBar1.Visible = false;
+            }
+            else{
+                MessageBox.Show("Arquivo nao encontrado para leitura");
+            }
+        }
+
+        private async void processarArquivo(string arquivo, string delimitador)
+        {
+            await LendoArquivo(arquivo, delimitador);
+
+
+        }
+
+        private async Task LendoArquivo(string arquivo, string delimitador)
+        {
+            try
+            {
+                string line = "";
+                StreamReader reader = new StreamReader(arquivo);
+                char delim = ' ';
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (!delimitador.Trim().Equals(""))
+                    {
+
+                        delim = Convert.ToChar(delimitador);
+                    }
+
+                    string[] palavras = line.Split(delim);
+                    foreach (var word in palavras)
+                    {
+                        if (word.Contains("<!$MG_"))
+                        {
+                            strVariaveis.Append(word + ";");
+                        }
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
